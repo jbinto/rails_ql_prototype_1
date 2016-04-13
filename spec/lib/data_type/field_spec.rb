4cb010cffc33fe6d387fa67bd62828e5aea70601
Stuart Garvagh <sgarvagh@gmail.com>
2016-04-13 15:51:25 -0400

    Field specs passing

--- spec/lib/data_type/field_spec.rb
+++ spec/lib/data_type/field_spec.rb
@@ -17,7 +17,8 @@ describe RailsQL::DataType::Field do
     described_class.new(
       field_definition: field_definition,
       data_type: data_type,
-      parent_data_type: parent_data_type
+      parent_data_type: parent_data_type,
+      name: "field_name"
     )
   end
 
@@ -50,6 +51,52 @@ describe RailsQL::DataType::Field do
   end
 
   describe "#resolved_model" do
+    context "when field_definition has a resolve defined" do
+      it "instance execs the field_definition#resolve in the context of the parent data type" do
+        args = double
+        query = double
+
+        expect(data_type).to receive(:args).and_return args
+        expect(data_type).to receive(:query).and_return query
+        field_definition_resolve = ->(actual_args, actual_query){
+          [actual_args, actual_query, self]
+        }
+        expect(field_definition).to receive(:resolve).twice.and_return(
+          field_definition_resolve
+        )
+
+        expect(field.resolved_model).to eq [args, query, parent_data_type]
+      end
+    end
+
+    context "when field_definition does not have a resolve defined" do
+      context "when parent_data_type has the field name defined as a method" do
+        it "returns parent_data_type#field_name" do
+          # don't use stubs because base does not define field_name
+          parent_data_type.instance_eval do
+            def field_name
+              "parent_model"
+            end
+          end
+          expect(field_definition).to receive(:resolve).and_return nil
+          expect(field.resolved_model).to eq "parent_model"
+        end
+      end
+
+      context "when parent_data_type does not have the field name defined as a method" do
+        it "returns parent_data_type.model#field_name" do
+          parent_model = double
+          expect(parent_model).to receive(:field_name).and_return(
+            "parent_model"
+          )
+          expect(parent_data_type).to receive(:model).and_return(
+            parent_model
+          )
+          expect(field_definition).to receive(:resolve).and_return nil
+          expect(field.resolved_model).to eq "parent_model"
+        end
+      end
+    end
   end
 
   describe "#has_read_permission?" do
