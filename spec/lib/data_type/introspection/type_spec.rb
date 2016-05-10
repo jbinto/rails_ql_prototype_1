d0a99eea4d6ab6fc8e0bb1653383e24af0b54f79
Rob Gilson <thatotherdude@gmail.com>
2016-05-10 11:48:35 -0400

    Add resolver spec to type

--- spec/lib/data_type/introspection/type_spec.rb
+++ spec/lib/data_type/introspection/type_spec.rb
@@ -2,6 +2,12 @@ require "spec_helper"
 
 describe RailsQL::DataType::Introspection::Type do
 
+  before(:each) do
+    allow_any_instance_of(described_class).to(
+      receive(:model).and_return data_type_klass
+    )
+  end
+
   let(:data_type_klass) do
     klass = Class.new RailsQL::DataType::Base
     klass.class_eval do
@@ -9,23 +15,49 @@ describe RailsQL::DataType::Introspection::Type do
         "Panda"
       end
       description "https://youtu.be/u155ncSlkCk"
-      # field :pandas_are_awesome, data_type: :Boolean
+      field :pandas_are_awesome, data_type: :Boolean
+      field :because_reasons, data_type: :Boolean
     end
     klass
   end
 
-  describe "recurse_over_data_type_klasses" do
-    it "returns an array with the name and description of every data_type" do
-      allow_any_instance_of(described_class).to(
-        receive(:model).and_return data_type_klass
-      )
-      runner = RailsQL::Runner.new described_class
+  let(:runner) {
+    runner = RailsQL::Runner.new described_class
+  }
 
-      results = runner.execute!(query: "query {name, description}").as_json
+  describe "[:name]" do
+    it "returns the data type's name" do
+      results = runner.execute!(query: "query {name}").as_json
 
       expect(results["name"]).to eq "Panda"
-      expect(results["description"]).to eq "https://youtu.be/u155ncSlkCk"
-      # expect(results["fields"]).to eq [a field for "pandas_are_awesome"]
     end
   end
+
+  describe "[:description]" do
+    it "returns the data type's description" do
+      results = runner.execute!(query: "query {description}").as_json
+
+      expect(results["description"]).to eq "https://youtu.be/u155ncSlkCk"
+    end
+  end
+
+  describe ".field_definitions" do
+    describe "[:fields]#resolve" do
+      it "resolves to a list of field definitions" do
+        field_definition = described_class.field_definitions[:fields]
+
+        results = field_definition.resolve(
+          parent_data_type: described_class.new
+        )
+
+        expect(results.length).to eq 2
+        expect(results.map &:name).to eq [
+          :pandas_are_awesome,
+          :because_reasons
+        ]
+      end
+    end
+  end
+  # expect(results["fields"]).to eq [a field for "pandas_are_awesome"]
+
 end
