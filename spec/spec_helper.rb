f4fca782ca7327b16d49709835a599bb9245b21e
Jesse Buchanan <hello@jessebuchanan.ca>
2016-07-26 17:01:21 -0400

    Use color in rspec output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,4 +15,12 @@ RSpec.configure do |config|
   RSpec::Expectations.configuration.warn_about_potential_false_positives = false
 
   config.order = "random"
+
+  # Use color in STDOUT
+  config.color = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+
 end
