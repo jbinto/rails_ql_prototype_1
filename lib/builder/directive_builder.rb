cadec0bd6e1527d6902832803dd96e2f95efc196
Rob Gilson <thatotherdude@gmail.com>
2016-08-03 23:54:24 -0400

    refactor: commenting out code that will not be in this iteration

--- lib/builder/directive_builder.rb
+++ lib/builder/directive_builder.rb
@@ -1,17 +1,19 @@
-module RailsQL
-  module Builder
-    class DirectiveBuilder
-      attr_accessor(
-        :name, :arg_builder
-      )
+# Pending dev time
 
-      def initialize(type_klass:)
-        @type_klass = ::RailsQL::Type::KlassFactory.find type_klass
-        @arg_type_builder = TypeBuilder.new(
-          type_klass: @type_klass.args_definition
-        )
-      end
-
-    end
-  end
-end
+# module RailsQL
+#   module Builder
+#     class DirectiveBuilder
+#       attr_accessor(
+#         :name, :arg_builder
+#       )
+#
+#       def initialize(type_klass:)
+#         @type_klass = ::RailsQL::Type::KlassFactory.find type_klass
+#         @arg_type_builder = TypeBuilder.new(
+#           type_klass: @type_klass.args_definition
+#         )
+#       end
+#
+#     end
+#   end
+# end
