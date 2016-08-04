3999c7faab32dffd108bbc51e63a98de4efc4513
Jesse Buchanan <hello@jessebuchanan.ca>
2016-08-04 15:32:20 -0400

    failing test and debug puts for permissions_check_executer.rb

--- lib/executers/permissions_check_executer.rb
+++ lib/executers/permissions_check_executer.rb
@@ -13,21 +13,31 @@ module RailsQL
 
       private
       def unauthorized_fields_and_args_for(action, parent:)
-        parent.query_tree_children.reduce({}) do |h, child|
+        # binding.pry
+        puts "BEGIN@@ reducing parent=#{parent}"
+        result = parent.query_tree_children.reduce({}) do |h, child|
+          puts "  parent=#{parent}  h=#{h} child=#{child}"
+          # binding.pry
           if parent.can? action, child.field_name
+            puts "  parent_can? #{action} #{child.field_name} => true"
             json = {}
+            puts "    BEGIN~~ recursion into nested fields of #{parent} (new_parent: child => #{child}"
             unauthorized_fields = unauthorized_fields_and_args_for(
               action,
               parent: child
             )
+            puts "    END~~ recursion into nested fields of #{parent}"
+            puts "    BEGIN!! recursion into args of #{parent} (new_parent: child.args_type => #{child.args_type})"
             unauthorized_args = unauthorized_fields_and_args_for(
               :input,
               parent: child.args_type
             )
+            puts "    END!! recursion into args of #{parent}"
 
             json.merge! *unauthorized_fields if unauthorized_fields.present?
             json["__args"] = unauthorized_args if unauthorized_args.present?
           else
+            puts "  parent_can? #{action} #{child.field_name} => false  "
             json = true
           end
 
@@ -35,6 +45,9 @@ module RailsQL
           h[child.field_name] = json if json.present?
           h
         end
+        puts "END@@ reducing parent=#{parent}"
+
+        result
       end
 
     end
--- spec/lib/executers/permissions_check_executer_spec.rb
+++ spec/lib/executers/permissions_check_executer_spec.rb
@@ -1,10 +1,10 @@
 require "spec_helper"
 
 describe RailsQL::Executers::PermissionsCheckExecuter do
-  let(:root) {instance_double RailsQL::Type}
+  let(:root) {instance_double RailsQL::Type, to_s: "RootTypeDouble"}
 
   def field_with_no_children_and_no_args(name:)
-    f = instance_double RailsQL::Type
+    f = instance_double RailsQL::Type, to_s: "FieldWithNoArgsTypeDouble_#{name}"
     allow(f).to receive(:field_name).and_return name
     allow(f).to receive(:query_tree_children).and_return []
     allow(f).to receive_message_chain(
@@ -12,6 +12,38 @@ describe RailsQL::Executers::PermissionsCheckExecuter do
       :query_tree_children
     ).and_return []
     f
+  end
+
+  def field_with_args(name:, args_can_input:)
+    f = instance_double RailsQL::Type, to_s: "FieldTypeDouble_#{name}"
+    expect(f).to receive(:field_name).and_return name
+    expect(f).to receive(:query_tree_children).and_return []
+
+    args = []
+    args_can_input.each do |name, can_input|
+      arg = instance_double RailsQL::Type, to_s: "ArgsTypeDouble_#{name}"
+      expect(arg).to receive(:field_name).and_return name
+
+      # Set the parent field's can?
+      expect(f).to receive_message_chain(:args_type, :can?).with(:input, name)
+        .and_return can_input
+
+      # Nothing nested inside args
+      allow(arg).to receive(:query_tree_children).and_return []
+      allow(arg).to receive_message_chain(
+        :args_type,
+        :query_type_children
+      ).and_return []
+
+      args << arg
+    end
+
+    allow(f).to receive_message_chain(
+      :args_type,
+      :query_tree_children
+    ).and_return(args)
+
+    return f
   end
 
   def test_executer(root:, expected:, operation_type: :query)
@@ -24,6 +56,31 @@ describe RailsQL::Executers::PermissionsCheckExecuter do
 
 
   describe "#unauthorized_fields_and_args_for" do
+    context "when there is an unauthorized arg" do
+      it %|returns a hash {field_name => "__args" => {arg_name => true}}| do
+        field = field_with_args({
+          name: "cow",
+          args_can_input: {
+            "moo" => true,
+            "restricted_moo" => false
+          }
+        })
+        allow(root).to receive(:query_tree_children).and_return [field]
+        allow(root).to receive(:can?).with(:query, "cow").and_return true
+
+        test_executer(
+          root: root,
+          expected: {
+            "cow" => {
+              "__args" => {
+                "restricted_moo" => true
+              }
+            }
+          }
+        )
+      end
+    end
+
     context "when there are unauthorized fields" do
       it "returns a hash in the form {field_name => true}" do
         field_1 = field_with_no_children_and_no_args(name: "cow")
