e7519fc4a372c9f16950cefc4959644eaaeb96aa
Jesse Buchanan <hello@jessebuchanan.ca>
2016-08-03 23:01:36 -0400

    fix: refactor spec for #unauthorized_fields_and_args_for

--- spec/lib/executers/permissions_check_executer_spec.rb
+++ spec/lib/executers/permissions_check_executer_spec.rb
@@ -14,9 +14,36 @@ describe RailsQL::Executers::PermissionsCheckExecuter do
     f
   end
 
+  def test_executer(root:, expected:, operation_type: :query)
+    executer = described_class.new(
+      root: root,
+      operation_type: operation_type
+    ).execute!
+    expect(executer.unauthorized_fields_and_args).to eq expected
+  end
+
+
   describe "#unauthorized_fields_and_args_for" do
+    context "when there are unauthorized fields" do
+      it "returns a hash in the form {field_name => true}" do
+        field_1 = field_with_no_children_and_no_args(name: "cow")
+        field_2 = field_with_no_children_and_no_args(name: "horse")
+        allow(root).to receive(:query_tree_children).and_return(
+          [field_1, field_2]
+        )
+
+        allow(root).to receive(:can?).with(:query, "cow").and_return false
+        allow(root).to receive(:can?).with(:query, "horse").and_return true
+
+        test_executer(
+          root: root,
+          expected: {"cow" => true}
+        )
+      end
+    end
+
     context "when there are no unauthorized fields" do
-      it "can be executed" do
+      it "returns an empty hash" do
         field_1 = field_with_no_children_and_no_args(name: "cow")
         field_2 = field_with_no_children_and_no_args(name: "horse")
         allow(root).to receive(:query_tree_children).and_return(
@@ -26,12 +53,10 @@ describe RailsQL::Executers::PermissionsCheckExecuter do
         allow(root).to receive(:can?).with(:query, "cow").and_return true
         allow(root).to receive(:can?).with(:query, "horse").and_return true
 
-        executer = described_class.new(root: root,
-          operation_type: :query
-        ).execute!
-
-        actual = executer.unauthorized_fields_and_args
-        expect(actual).to eq({})
+        test_executer(
+          root: root,
+          expected: {}
+        )
       end
     end
   end
