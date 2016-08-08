b8054997c5cbce5a7ac11d09fd0b58c2518b4a2f
Jesse Buchanan <hello@jessebuchanan.ca>
2016-08-08 14:40:18 -0400

    spec: QueryExecuter complete spec

--- lib/executers/query_executer.rb
+++ lib/executers/query_executer.rb
@@ -17,13 +17,14 @@ module RailsQL
         child_query_nodes_for(child).each do |grandchild_node|
           recurse_into_child grandchild_node
         end
-        # if child.query_lambda.present?
-        #   parent.query = parent.instance_exec(
-        #     child.args,
-        #     parent.query,
-        #     &child.query_lambda
-        #   )
-        # end
+        if child.query_lambda.present?
+          puts "child.query_lambda.present? == true (for parent=#{parent}, child=#{child})"
+          parent.query = parent.instance_exec(
+            child.args,
+            child.query,
+            &child.query_lambda
+          )
+        end
       end
 
     end
--- spec/lib/executers/query_executer_spec.rb
+++ spec/lib/executers/query_executer_spec.rb
@@ -1,7 +1,5 @@
 require "spec_helper"
 
-
-
 describe RailsQL::Executers::QueryExecuter do
   def run_query_executer_test(root:, operation_type: :query)
     described_class.new(
@@ -11,34 +9,50 @@ describe RailsQL::Executers::QueryExecuter do
   end
 
   describe '#execute!' do
-    context "with child nodes" do
-      def make_node(name)
-        instance_double RailsQL::Type, name
-      end
+    def make_node(name)
+      instance_double RailsQL::Type, name
+    end
 
-      def node_with_no_children(name: "NodeWithNoChildren")
-        node = make_node name
-        expect(node).to receive(:query_tree_children).and_return([])
-        return node
-      end
+    def node_with_no_children(name: "NodeWithNoChildren")
+      node = make_node name
+      expect(node).to receive(:query_tree_children).and_return([])
+      return node
+    end
 
-      def node_with_children(children, name: "NodeWithChildren")
-        node = make_node name
-        expect(node).to receive(:query_tree_children).and_return(children)
-        return node
-      end
+    def node_with_children(children, name: "NodeWithChildren")
+      node = make_node name
+      expect(node).to receive(:query_tree_children).and_return(children)
+      return node
+    end
 
-      def expect_initial_query(on:, with:)
-        expect(on).to receive(:initial_query).and_return with
-        expect(on).to receive(:query=).with(with)
-      end
+    def expect_initial_query(on:, with:)
+      expect(on).to receive(:initial_query).and_return with
+      expect(on).to receive(:query=).with(with)
+    end
 
+    def ignore_query_lambda(on:)
+      allow(on).to receive(:query_lambda).and_return nil
+    end
+
+    def ignore_initial_query(on:)
+      allow(on).to receive(:initial_query).and_return "initial_query"
+      allow(on).to receive(:query=).with "initial_query"
+    end
+
+    def ignore_args(on:)
+      allow(on).to receive(:args).and_return({})
+    end
+
+    context "initial query" do
       it "sets initial query on root" do
         # e.g. { root }
         root = node_with_no_children name: "Root"
 
         expect(root).to receive(:initial_query).and_return nil
         expect(root).to receive(:query=).with(nil)
+
+        # tangential to this test
+        ignore_query_lambda(on: root)
 
         run_query_executer_test(root: root)
       end
@@ -51,10 +65,14 @@ describe RailsQL::Executers::QueryExecuter do
         expect_initial_query(on: root, with: nil)
         expect_initial_query(on: field_1, with: "Moo.all")
 
+        # tangential to this test
+        ignore_query_lambda(on: root)
+        ignore_query_lambda(on: field_1)
+
         run_query_executer_test(root: root)
       end
 
-      it "sets initial query on children one-level deep" do
+      it "sets initial query on children two-levels deep" do
         # e.g. { root { field_1 { field_1_a }}}
         field_1_a = node_with_no_children name: "Field_1_a"
         field_1 = node_with_children [field_1_a], name: "Field_1"
@@ -63,6 +81,82 @@ describe RailsQL::Executers::QueryExecuter do
         expect_initial_query(on: root, with: nil)
         expect_initial_query(on: field_1, with: "Field_1.all")
         expect_initial_query(on: field_1_a, with: "Field_1_a.all")
+
+        # tangential to this test
+        ignore_query_lambda(on: root)
+        ignore_query_lambda(on: field_1)
+        ignore_query_lambda(on: field_1_a)
+
+        run_query_executer_test(root: root)
+      end
+    end
+
+    context "execution of lambda" do
+      def stub_query_lambda(on:, name:)
+        allow(on).to receive(:query_lambda).and_return(
+          ->(args, child_query){{
+            name: "#{name}_query_lambda",
+            args: args,
+            child_query: child_query,
+            self: self
+          }}
+        )
+      end
+
+      def stub_query_lambda_with_nil(on:)
+        expect(on).to receive(:query_lambda).and_return nil
+      end
+
+      def stub_query_var(on:, query:)
+        expect(on).to receive(:query).and_return(query)
+      end
+
+      it "executes query_lambda in the context of its parent" do
+        # e.g. { root { drinks { beers }}
+        # The query lambda defined on `beers` executes in `drink`s context.
+        # The query lambda defined on `drinks` executes in `root`s context.
+        # No query lambda should ever be defined on a root.
+
+        beers = node_with_no_children name: "Beers"
+        drinks = node_with_children [beers], name: "Drinks"
+        root = node_with_children [drinks], name: "Root"
+
+        # XXX TODO: for now, stubbed types just return canned args
+        # this would be for e.g. { root { drinks(promos: true) { beers }}}
+        ignore_args(on: beers)
+        ignore_args(on: drinks)
+
+        # use a canned query_lambda on every type that just returns
+        # info about how it was called.
+        stub_query_lambda(on: beers, name: "beers")
+        stub_query_lambda(on: drinks, name: "drinks")
+        stub_query_lambda_with_nil(on: root)
+
+        # e.g. the initial_query of each type
+        #  (note: since we're stubbing we can't just set #initial_query,
+        #  we have to do it a bit awkwardly)
+        stub_query_var(on: beers, query: "Beers.all")
+        stub_query_var(on: drinks, query: "Drinks.all")
+
+        # omg an actual assertion!
+        expect(drinks).to receive(:query=).with({
+          name: "beers_query_lambda",
+          args: {},
+          child_query: "Beers.all",
+          self: drinks
+        })
+
+        expect(root).to receive(:query=).with({
+          name: "drinks_query_lambda",
+          args: {},
+          child_query: "Drinks.all",
+          self: root
+        })
+
+        # tangential to this test
+        ignore_initial_query(on: root)
+        ignore_initial_query(on: drinks)
+        ignore_initial_query(on: beers)
 
         run_query_executer_test(root: root)
       end
