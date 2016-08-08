6d551d123c10e93c73e9a3f67db5eeba81ff36f4
Jesse Buchanan <hello@jessebuchanan.ca>
2016-08-08 15:42:48 -0400

    fix: change ResolveExecuter (back) to recursion

--- lib/executers/resolve_executer.rb
+++ lib/executers/resolve_executer.rb
@@ -3,31 +3,31 @@ module RailsQL
     class ResolveExecuter < Executer
 
       def execute!
-        stack = child_resolve_nodes_for @root
-
-        # Iterate from the root type to the scalar leaves of the type tree
-        stack.each do |node|
-          child, parent = node.slice(:child, :parent)
-
-          stack << child_resolve_nodes_for(child)
-          child.model =
-            if child.resolve_lambda.present?
-              # todo: Directives could make use of around_resolve hooks to
-              # stop resolution if there were hooks here.
-              # eg. parent.trigger :around_resolve do ... end
-              parent.instance_exec(
-                child.args,
-                parent.query,
-                child.resolve_lambda
-              )
-            else
-              default_resolve_for! node
-            end
-        end
+        child_resolve_nodes_for(@root).each {|node| recurse_into_child! node}
         return self
       end
 
       private
+
+      def recurse_into_child!(parent:, child:)
+        # Iterate top down - from the root type to the scalar leaves of the
+        # type tree
+        child.model =
+          if child.resolve_lambda.present?
+            # todo: Directives could make use of around_resolve hooks to
+            # stop resolution if there were hooks here.
+            # eg. parent.trigger :around_resolve do ... end
+            parent.instance_exec(
+              child.args,
+              parent.query,
+              child.resolve_lambda
+            )
+          else
+            default_resolve_for! node
+          end
+        # add the children to the stack after the parent has been resolved
+        child_resolve_nodes_for(child).each {|node| recurse_into_child! node}
+      end
 
       def default_resolve_for!(child:, parent:)
         name = child.name
