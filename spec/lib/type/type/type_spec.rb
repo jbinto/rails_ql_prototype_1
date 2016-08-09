f403d9760af31c44b6e78dc9cd726ce787f40659
Jesse Buchanan <hello@jessebuchanan.ca>
2016-08-09 16:06:15 -0400

    spec: RailsQL::Type (WIP)

--- spec/lib/type/type/type_spec.rb
+++ spec/lib/type/type/type_spec.rb
@@ -196,14 +196,18 @@ describe RailsQL::Type do
         end
 
         url = new_string_klass "url"
+        url.model = "http://example.org/"
         images = new_object_klass "images", field_types: { "url" => url }
         hats = new_object_klass "hats", field_types: { "images" => images }
         products = new_object_klass "products", field_types: { "hats" => hats }
 
-        pending
-        fail
-
-
+        expect(products.as_json).to eq({
+          "hats" => {
+            "images" => {
+              "url" => "http://example.org/"
+            }
+          }
+        })
       end
     end
   end
@@ -285,122 +289,3 @@ describe RailsQL::Type do
     end
   end
 end
-
-
-
-#   describe "#build_query!" do
-#     context "when it has no fields" do
-#       it "returns the initial query" do
-#         allow(type_klass).to receive(:get_initial_query).and_return(
-#           ->{:best_query_ever}
-#         )
-#         expect(type_klass.new.build_query!).to eq :best_query_ever
-#       end
-#     end
-#
-#     context "when it has fields" do
-#       it "calls Field#appended_parent_query and saves results to query" do
-#         field = instance_double RailsQL::Field::Field
-#         type = type_klass.new
-#         allow(type).to receive(:fields).and_return(fake_field: field)
-#         child_type = instance_double described_class
-#         allow(field).to receive(:prototype_type).and_return(
-#           child_type
-#         )
-#         allow(child_type).to receive :build_query!
-#         allow(type_klass).to receive(:get_initial_query).and_return double
-#
-#         expect(field).to receive(:appended_parent_query).and_return :lions
-#         type.build_query!
-#         expect(type.query).to eq :lions
-#       end
-#
-#       it "reduces over multiple fields via Field#appended_parent_query" do
-#         fields = {
-#           fake_field_1: instance_double(RailsQL::Field::Field),
-#           fake_field_2: instance_double(RailsQL::Field::Field)
-#         }
-#         allow(type_klass).to receive(:get_initial_query).and_return(
-#           -> {"the cow says"}
-#         )
-#         type = type_klass.new
-#         allow(type).to receive(:fields).and_return fields
-#
-#         fields.each do |k, field|
-#           allow(field).to(
-#             receive_message_chain(:prototype_type, :build_query!).and_return(
-#               double
-#             )
-#           )
-#           expect(field).to receive(:appended_parent_query) do
-#             type.query + " moo"
-#           end.once
-#         end
-#         expect(type.build_query!).to eq "the cow says moo moo"
-#       end
-#     end
-#   end
-#
-#   describe "#resolve_child_types!" do
-#     before :each do
-#       @type = type_klass.new
-#       @field = instance_double RailsQL::Field::Field
-#       allow(@type).to receive(:fields).and_return(fake_field: @field)
-#       allow(@field).to receive :parent_type=
-#       allow(@field).to receive :resolve_models_and_dup_type!
-#       allow(@field).to receive(:types).and_return []
-#     end
-#
-#     it "assigns self as the parent_type to each field" do
-#       expect(@field).to receive(:parent_type=).with @type
-#
-#       @type.resolve_child_types!
-#     end
-#
-#     it "calls Field#resolve_models_and_dup_type! for each field" do
-#       expect(@field).to receive :resolve_models_and_dup_type!
-#       @type.resolve_child_types!
-#     end
-#
-#     it "calls resolve_child_types! on child_types" do
-#       field_type = instance_double described_class
-#       allow(@field).to receive(:types).and_return [field_type]
-#
-#       expect(field_type).to receive :resolve_child_types!
-#
-#
-#       @type.resolve_child_types!
-#     end
-#
-#     it "runs resolve callbacks" do
-#       expect do |b|
-#         type_klass.before_resolve &b
-#         @type.resolve_child_types!
-#       end.to yield_control
-#     end
-#
-#   end
-#
-#   describe "#as_json" do
-#     context "when kind is defaulted to :OBJECT" do
-#       it "reduces over #as_json on fields" do
-#         field = instance_double RailsQL::Field::Field
-#         allow(field).to receive(:singular?).and_return true
-#         type = type_klass.new
-#         allow(type).to receive(:fields).and_return(
-#           fake_field_1: field,
-#           fake_field_2: field
-#         )
-#         allow(field).to receive_message_chain(:types, :as_json).and_return(
-#           ["hello" => "world"]
-#         )
-#
-#         expect(type.as_json).to eq(
-#           "fake_field_1" => {"hello" => "world"},
-#           "fake_field_2" => {"hello" => "world"}
-#         )
-#       end
-#     end
-#
-#   end
-# end
