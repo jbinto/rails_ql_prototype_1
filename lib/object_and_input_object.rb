5b566d375623f6aa3d1480ee3d6daed154ac8cf6
Rob Gilson <thatotherdude@gmail.com>
2016-08-10 11:30:14 -0400

    Add input_object_and_object helper

---
+++ lib/object_and_input_object.rb
@@ -0,0 +1,14 @@
+module RailsQL
+  def object_and_input_object(&block)
+    container = Module.new
+    container.const_set :ObjectType, Class.new(RailsQL::Type) do
+      kind :object
+    end
+    container.const_set :InputObjectType, Class.new(RailsQL::Type) do
+      kind :input_object
+    end
+    instance_eval container::ObjectType, &block
+    instance_eval container::InputObjectType, &block
+    return container
+  end
+end
