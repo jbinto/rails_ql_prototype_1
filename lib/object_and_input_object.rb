3beab6d4a6e48a143f8d90f127098103c4f20c3b
Rob Gilson <thatotherdude@gmail.com>
2016-08-10 14:25:03 -0400

    fix: revert object_and_input_object class names fix

--- lib/object_and_input_object.rb
+++ lib/object_and_input_object.rb
@@ -4,18 +4,10 @@ module RailsQL
 
     container.const_set :ObjectType, Class.new(RailsQL::Type) do
       kind :object
-
-      def self.name
-        "ObjectType"
-      end
     end
 
     container.const_set :InputObjectType, Class.new(RailsQL::Type) do
       kind :input_object
-
-      def self.name
-        "InputObjectType"
-      end
     end
 
     container::ObjectType.instance_exec &block
