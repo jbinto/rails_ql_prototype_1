985f0f3e5b1d08bcf27600335706d83e4b92b16c
Rob Gilson <thatotherdude@gmail.com>
2016-08-10 14:19:32 -0400

    fix: object_and_input_object class names

--- lib/object_and_input_object.rb
+++ lib/object_and_input_object.rb
@@ -1,18 +1,23 @@
 module RailsQL
   def self.object_and_input_object(&block)
     container = Module.new
+
     container.const_set :ObjectType, Class.new(RailsQL::Type) do
       kind :object
-      def name
+
+      def self.name
         "ObjectType"
       end
     end
+
     container.const_set :InputObjectType, Class.new(RailsQL::Type) do
       kind :input_object
-      def name
+
+      def self.name
         "InputObjectType"
       end
     end
+
     container::ObjectType.instance_exec &block
     container::InputObjectType.instance_exec &block
     return container
